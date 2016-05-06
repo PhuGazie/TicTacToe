165c0b5cc0f0bf251ea62c50a379ea9de4d67626
PhuGazie <jennyphu@gmail.com>
2016-05-05 18:38:19 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $(document).on("click", ".game-space", function() {
+    $(this).addClass("add-X");
+  });
+
+  $(document).on("click", ".game-space", function(){
+      $(this)
+      .html("<p>X</p>");
+  });
+
+
+var player1= "X"
+var player2= "O"
+
+
+});
